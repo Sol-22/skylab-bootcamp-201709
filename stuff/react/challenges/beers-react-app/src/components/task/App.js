f48b1b0639355f06f7cead5f074557bb4a4c4dbf
Sol-22 <sol.noeli.lopez@gmail.com>
2017-10-25 11:30:19 +0200

    update

---
+++ stuff/react/challenges/beers-react-app/src/components/task/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Tasks from './components/Tasks'
+
+class App extends Component {
+  render() {
+    return (
+        <Tasks/>
+    )
+  }
+}
+
+export default App
